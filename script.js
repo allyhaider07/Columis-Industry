5ae96add2e423ec3abf2c3c23a66c93fccafd515
Haiderally <haideraally@gmail.com>
2023-10-26 16:28:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var counter = 1;
+// setInterval(function(){
+//  document.getElementById('radio' + counter).checked = true ;
+//  if (counter > 4) {
+//     counter = 1;
+//  } 
+// }, 5000);
+
+let ch = document.getElementById("ch")
+ch.style.color = "pink"
